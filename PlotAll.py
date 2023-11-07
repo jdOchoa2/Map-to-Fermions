eaa96854102567831106ea7f076d4a6194d5ef4e
Juan David Ochoa Acosta <juochoaa@unal.edu.co>
2023-11-06 22:36:34 -0500

    ERROR 404

--- PlotAll.py
+++ PlotAll.py
@@ -30,7 +30,7 @@ def read_correlation(J_min, distribution):
     Var_C_xx = df['Var_C_xx'].to_numpy()
     C_xxpp = df['C_xxpp'].to_numpy()
     Var_C_xxpp = df['Var_C_xxpp'].to_numpy()
-    return [-R**2 * C_zz,R**2 * np.sqrt(Var_C_zz),R**2 * np.abs(C_xx),R**2 * np.sqrt(Var_C_xx),np.abs(C_xx + C_xxpp),R]
+    return [-R**2 * C_zz,R**2 * np.sqrt(Var_C_zz),R**2 * np.abs(C_xx),R**2 * np.sqrt(Var_C_xx),np.abs(C_xx + C_xxpp), Var_C_xxpp, R]
 
 def Which_Model(Model):
     if Model == "1":
@@ -40,14 +40,14 @@ def Which_Model(Model):
 
 """---------------MAIN-----------------"""
 
-SeriesX = [None,None,None]; ErrorX = [None,None,None]; SeriesXpp = [None,None,None]
-SeriesZ = [None,None,None]; ErrorZ = [None,None,None]
+SeriesX = [None,None,None]; SeriesZ = [None,None,None]; SeriesXpp = [None,None,None]
+ErrorX  = [None,None,None];  ErrorZ = [None,None,None];  ErrorXpp = [None,None,None]
 
 J_min = [None,None,None]  ; distribution = [None,None,None]; R = []
 
 for n in range(3):
     J_min[n], distribution[n] = read_parameters(n+1)
-    SeriesZ[n], ErrorZ[n], SeriesX[n], ErrorX[n], SeriesXpp[n], R = read_correlation(J_min[n], distribution[n])
+    SeriesZ[n], ErrorZ[n], SeriesX[n], ErrorX[n], SeriesXpp[n], ErrorXpp[n], R = read_correlation(J_min[n], distribution[n])
 
 # Plotting C_zz
 plt.figure(figsize=(5, 5))
@@ -65,6 +65,19 @@ plt.ylabel("$-l^2C_{zz}(l)$")
 plt.ylim(0, 0.25)
 plt.legend(loc="upper left")
 plt.savefig("./Images/Czz.png", dpi=300, bbox_inches="tight")
+
+# Plotting DesvEsta C_zz
+plt.figure(figsize=(5, 1))
+plt.scatter(R, np.sqrt(ErrorZ[0]), color="red", s=4)
+plt.plot(R,    np.sqrt(ErrorZ[0]), linestyle='--', linewidth=0.5, color='red')
+plt.scatter(R, np.sqrt(ErrorZ[1]), color="green", s=4)
+plt.plot(R,    np.sqrt(ErrorZ[1]), linestyle='--', linewidth=0.5, color='green')
+plt.scatter(R, np.sqrt(ErrorZ[2]), color="blue", s=4)
+plt.plot(R,    np.sqrt(ErrorZ[2]), linestyle='--', linewidth=0.5, color='blue')
+plt.xscale('log')
+plt.xlabel("$l$")
+plt.ylabel("$\sigma$")
+plt.savefig("./Images/Errorzz.png", dpi=300, bbox_inches="tight")
 
 # Plotting C_xx
 x = np.arange(1, 201, 2); f = lambda x: 0.14709 * x**(3/2); y = f(x) #Clean system
@@ -85,6 +98,20 @@ plt.ylim(0.05, 300)
 plt.legend(loc="upper left")
 plt.savefig("./Images/Cxx.png", dpi=300, bbox_inches="tight")
 
+# Plotting DesvEsta C_xx
+plt.figure(figsize=(5, 1))
+plt.scatter(R, np.sqrt(ErrorX[0]), color="red", s=4)
+plt.plot(R,    np.sqrt(ErrorX[0]), linestyle='--', linewidth=0.5, color='red')
+plt.scatter(R, np.sqrt(ErrorX[1]), color="green", s=4)
+plt.plot(R,    np.sqrt(ErrorX[1]), linestyle='--', linewidth=0.5, color='green')
+plt.scatter(R, np.sqrt(ErrorX[2]), color="blue", s=4)
+plt.plot(R,    np.sqrt(ErrorX[2]), linestyle='--', linewidth=0.5, color='blue')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel("$l$")
+plt.ylabel("$\sigma$")
+plt.savefig("./Images/Errorxx.png", dpi=300, bbox_inches="tight")
+
 #Plotting C_xx + C_xx+1
 x = np.arange(1, 201, 2); f = lambda x: 1/(12*x**(2)); y = f(x) #Universallity prediction
 plt.figure(figsize=(5, 5))
@@ -102,3 +129,17 @@ plt.ylabel("$|C_{xx}(l)+C_{xx}(l+1)|$")
 plt.ylim(10e-8,.1)
 plt.legend(loc="lower left")
 plt.savefig("./Images/Cxxpp.png", dpi=300, bbox_inches="tight")
+
+# Plotting DesvEsta C_xx + C_xx+1
+plt.figure(figsize=(5, 1))
+plt.scatter(R, np.sqrt(ErrorXpp[0]), color="red", s=4)
+plt.plot(R,    np.sqrt(ErrorXpp[0]), linestyle='--', linewidth=0.5, color='red')
+plt.scatter(R, np.sqrt(ErrorXpp[1]), color="green", s=4)
+plt.plot(R,    np.sqrt(ErrorXpp[1]), linestyle='--', linewidth=0.5, color='green')
+plt.scatter(R, np.sqrt(ErrorXpp[2]), color="blue", s=4)
+plt.plot(R,    np.sqrt(ErrorXpp[2]), linestyle='--', linewidth=0.5, color='blue')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel("$l$")
+plt.ylabel("$\sigma$")
+plt.savefig("./Images/Errorxxpp.png", dpi=300, bbox_inches="tight")
